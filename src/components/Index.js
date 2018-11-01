7e0783ce9cf5d8d197bffa56c6c6f70f056f5486
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-11-01 17:14:37 +0300

    First commit

---
+++ src/components/Index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class HomePageComponent extends Component {
+    state = {  }
+    render() {
+        return ( <div><h1>Welcome to Sokoni </h1></div> );
+    }
+}
+
+export default HomePageComponent;
